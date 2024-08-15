6ecbb06f3aff63e3dfaa030430de62c2ebd43e92
Vijay <vijaymce050@gmail.com>
2024-08-15 21:32:46 +0100

    First commit

---
+++ src/main/java/MavenTest5/MavenTest5/App.java
@@ -0,0 +1,13 @@
+package MavenTest5.MavenTest5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
